da1994c28de0e5a135ae6fdff985f109018be9cd
Vlad <gluk256@gmail.com>
2018-11-21 12:26:34 +0400

    fixed: input mode, file creation, flags

--- cmd/xkey/main.go
+++ cmd/xkey/main.go
@@ -8,9 +8,7 @@ import (
 	"math/big"
 	"os"
 	"strings"
-	//"os/user"
 
-	//"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/accounts/keystore"
 	"github.com/ethereum/go-ethereum/cmd/utils"
 	"github.com/ethereum/go-ethereum/console"
@@ -28,6 +26,7 @@ var (
 	eip155     bool
 	brainSrc   bool
 	extended   bool
+	plaintext  bool
 )
 
 func main() {
@@ -41,7 +40,7 @@ func main() {
 			help()
 			return
 		}
-		if strings.Contains(flags, "z") {
+		if strings.Contains(flags, "t") {
 			test()
 			return
 		}
@@ -56,11 +55,14 @@ func main() {
 		if strings.Contains(flags, "n") {
 			eip155 = true
 		}
-		if strings.Contains(flags, "t") {
+		if strings.Contains(flags, "f") {
 			txFromFile = true
 		}
 		if strings.Contains(flags, "b") {
 			brainSrc = true
+		}
+		if strings.Contains(flags, "p") {
+			plaintext = confirmPlainTextMode()
 		}
 	}
 	run()
@@ -70,18 +72,24 @@ func help() {
 	fmt.Println("xkey v.0.3")
 	fmt.Println("USAGE: xkey [flags]")
 	fmt.Println("\t h - help")
-	fmt.Println("\t x - extended input")
-	fmt.Println("\t i - import brain wallet")
+	fmt.Println("\t i - import brainwallet")
+	fmt.Println("\t b - use brainwallet")
 	fmt.Println("\t n - use network id")
-	fmt.Println("\t t - tx from file")
-	fmt.Println("\t b - brain wallet")
-	fmt.Println("\t z - test")
+	fmt.Println("\t f - tx from file")
+	fmt.Println("\t x - extended input")
+	fmt.Println("\t p - plaintext input")
+	fmt.Println("\t t - test")
+}
+
+func confirmPlainTextMode() bool {
+	s := getString("Do you really want the plain text mode? ")
+	return s[0] == 'y'
 }
 
 func getInt(text string) int {
+	fmt.Print(text)
 	var n int
 	for {
-		fmt.Print(text)
 		_, err := fmt.Scanf("%d", &n)
 		if err != nil {
 			fmt.Printf("error [%s], please try again \n", err)
@@ -93,17 +101,17 @@ func getInt(text string) int {
 }
 
 func getString(text string) string {
-	var s string
+	fmt.Print(text)
+	var s []byte
 	for {
-		fmt.Print(text)
-		_, err := fmt.Scanf("%s", &s)
-		if err != nil {
-			fmt.Printf("error [%s], please try again \n", err)
+		s = terminal.PlainTextInput()
+		if s == nil {
+			fmt.Print("please try again: ")
 		} else {
 			break
 		}
 	}
-	return s
+	return string(s)
 }
 
 func getSigner() types.Signer {
@@ -158,7 +166,7 @@ func ImportBrainWallet() {
 		utils.Fatalf("Unexpected error: file [%s] already exist", name)
 	}
 
-	if err := ioutil.WriteFile(name, keyjson, 0770); err != nil {
+	if err := ioutil.WriteFile(name, keyjson, 0666); err != nil {
 		utils.Fatalf("Failed to write keyfile to [%s]: %v", name, err)
 	}
 
@@ -166,6 +174,7 @@ func ImportBrainWallet() {
 	fmt.Println("key file: ", name)
 }
 
+// do not delete
 //func findFileByAddress(src common.Address) string {
 //	address := fmt.Sprintf("%x", src)
 //	usr, err := user.Current()
@@ -192,7 +201,20 @@ func ImportBrainWallet() {
 //	return string(name)
 //}
 
+func checkTxParams(args *ethapi.SendTxArgs) {
+	if args.Nonce == nil {
+		utils.Fatalf("Invalid tx: nonce is nil")
+	}
+	if args.To == nil {
+		utils.Fatalf("Invalid tx: [to] is nil")
+	}
+	if args.Gas == nil {
+		utils.Fatalf("Invalid tx: gas is nil")
+	}
+}
+
 func createTransaction(args *ethapi.SendTxArgs) *types.Transaction {
+	checkTxParams(args)
 	var input []byte
 	if args.Data != nil {
 		input = *args.Data
@@ -235,16 +257,17 @@ func getTransaction() *types.Transaction {
 	return tx
 }
 
-func getPassword(secure bool) string {
-	if secure {
-		p := terminal.SecureInput(extended)
-		return string(p)
+func getPassword() string {
+	if plaintext {
+		pass, err := console.Stdin.PromptPassword("Please enter password: ")
+		if err != nil {
+			utils.Fatalf("Failed to read passphrase: %v", err)
+		}
+		return pass
 	}
-	pass, err := console.Stdin.PromptPassword("Please enter password: ")
-	if err != nil {
-		utils.Fatalf("Failed to read passphrase: %v", err)
-	}
-	return pass
+
+	p := terminal.SecureInput(extended)
+	return string(p)
 }
 
 func bip39toKey(confirmations int) *keystore.Key {
@@ -269,14 +292,14 @@ func bip39toKey(confirmations int) *keystore.Key {
 }
 
 func getKeyFromFile() *keystore.Key {
-	//keyfile := findFileByAddress(gTxArgs.From)
+	//keyfile := findFileByAddress(gTxArgs.From) // do not delete
 	keyfile := getString("Please enter the key file name: ")
 	keyjson, err := ioutil.ReadFile(keyfile)
 	if err != nil {
 		utils.Fatalf("Failed to read the keyfile at '%s': %v", keyfile, err)
 	}
 
-	passphrase := getPassword(extended)
+	passphrase := getPassword()
 	key, err := keystore.DecryptKey(keyjson, string(passphrase))
 	if err != nil {
 		utils.Fatalf("Error decrypting key: %v", err)
@@ -311,11 +334,5 @@ func run() {
 }
 
 func test() {
-	for {
-		n := getString("test: ")
-		fmt.Printf("%v\n", n)
-		if n == "q" {
-			break
-		}
-	}
+	fmt.Println("test success")
 }
